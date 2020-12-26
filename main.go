ff1c6780ef31f6c16feea1b73f9cea5a2d5c145d
Takao Shibata <chise.alter.pasta@gmail.com>
2020-12-26 13:45:04 +0900

    kubebuilder create api --group virtualservicecomponent --version v1alpha1 --kind VirtualServiceBase

--- main.go
+++ main.go
@@ -74,6 +74,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "HTTPRouteBinding")
 		os.Exit(1)
 	}
+	if err = (&controllers.VirtualServiceBaseReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("VirtualServiceBase"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VirtualServiceBase")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
